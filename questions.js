3a08b0223969e0f6b8e5794b9512fac5a6277690
thomas <thomas.clerc@efrei.net>
2019-06-03 19:27:03 +0200

    ajout des questions
    revolution (no la pagamos, no la crisis)

---
+++ questions.js
@@ -0,0 +1,74 @@
+const myQuestions = [
+  {
+    question: "Où le célèbre révolutionnaire Marat fut assassiné ?",
+    answers: {
+      a: "dans sa beignoire",
+      b: "dans son lit",
+      c: "à l'assemblée"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Qui appelle-t-on le fils de la révolution ?",
+    answers: {
+      a: "Louis XVII",
+      b: "Napoléon Ier",
+      c: "Danton"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Quand fut proclammée la première République ?",
+    answers: {
+      a: "21 septembre 1792",
+      b: "4 aout 1789",
+      c: "14 juillet 1789"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Quel lieu parisien fut entièrement détruit par les révolutionnaires ?",
+    answers: {
+      a: "Le pont Neuf",
+      b: "Le palais des tuilleries",
+      c: "La prison de la Bastille"
+    },
+    correctAnswer: "c"
+  }
+  {
+    question: "Quel révolutionnaire eut pour derniers mots : '"'j"'"emporte avec moi les derniers lambeaux de la monarchie'"'?",
+    answers: {
+      a: "Danton",
+      b: "Mirabeau",
+      c: "Roberpierre"
+    },
+    correctAnswer: "b"
+  }
+  {
+    question: "Quel fut le premier débat qui mena à l'apparition du clivage droite gauche en France ?",
+    answers: {
+      a: "le droit de véto du roi",
+      b: "la mort du roi",
+      c: "l'imposition des nobles"
+    },
+    correctAnswer: "a"
+  }
+  {
+    question: "Où fut emprisonnée la famille royale ?",
+    answers: {
+      a: "dans la prision de la bastille",
+      b: "dans la prison du temple",
+      c: "au palais de justice"
+    },
+    correctAnswer: "b"
+  }
+  {
+    question: "Qui a inventé la guillotine ?",
+    answers: {
+      a: "Joseph Ignace Guillotin",
+      b: "Louis XVI",
+      c: "L'abbé Syes"
+    },
+    correctAnswer: "a"
+  }
+];
