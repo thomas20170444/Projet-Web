b06ceb9853d598f66ea251dfa96d2eb1b6ecd4d2
thomas <thomas.clerc@efrei.net>
2019-06-16 20:36:03 +0200

    Renaissance Antiquité

--- questions.js
+++ questions.js
@@ -88,5 +88,192 @@ const myQuestions = [
       c: "L'abbé Syes"
     },
     correctAnswer: "a"
+  },
+
+
+  /* Antiquité */
+
+  {
+    question: "Quand a eu lieu la conquête de la Gaule par Jules César ?",
+    answers: {
+      a: "en -300 av-JC",
+      b: "en -52 av-JC",
+      c: "en -78 av-JC"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Quelle est la dernière hypothèse quant à l'implantation de la Lutèce gauloise ?",
+    answers: {
+      a: "à Toulouse",
+      b: "à Paris",
+      c: "à Nanterre"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "À quoi une amphore servait-elle ?",
+    answers: {
+      a: "à des pratiques funéraires",
+      b: "à lester les bateaux",
+      c: "à transporter de la nourriture"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Quelle était la sauce préférée des Romains ?",
+    answers: {
+      a: "le jus de poisson fermenté",
+      b: "la sauce tomate",
+      c: "la sauce au sang"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Quelle est la hauteur du Pont du Gard ?",
+    answers: {
+      a: "49 mètres",
+      b: "23 mètres",
+      c: "9 mètres"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Quel est le volcan dont l’éruption a détruit Pompéi ?",
+    answers: {
+      a: "le Stromboli",
+      b: "le Vésuve",
+      c: "l'Etna"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Quelle culture étrangère imprégna la civilisation romaine ?",
+    answers: {
+      a: "la culture indienne",
+      b: "la culture grecque",
+      c: "la culture du maïs"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "A quel âge les jeunes spartiates étaient-ils confiés à l'Etat pour devenir des soldats ?",
+    answers: {
+      a: "13 ans",
+      b: "7 ans",
+      c: "17 ans"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Où se trouve Carthage ?",
+    answers: {
+      a: "en Espagne",
+      b: "en Afrique",
+      c: "en Bretagne"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Dans la Grèce antique, où avaient lieu les jeux les plus importants ?",
+    answers: {
+      a: "à Delphes",
+      b: "à Olympie",
+      c: "à Athènes"
+    },
+    correctAnswer: "b"
+  },
+
+
+  /* Renaissance */
+  {
+    question: "Qui a inventé le premier plan d'hélicoptère ?",
+    answers: {
+      a: "Léonard de Vincie",
+      b: "Galilée",
+      c: "Copernic"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Quel fut le deuxième roi français de la renaissance ?",
+    answers: {
+      a: "Henri II",
+      b: "François II",
+      c: "Henri III"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Quel fut le dernier roi de la renaissance ?",
+    answers: {
+      a: "Henri III",
+      b: "Henri IV",
+      c: "Louis Philippe"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Quel était la profession des memebres de la famille Médicis ?",
+    answers: {
+      a: "Tailleurs",
+      b: "Religieux",
+      c: "Banquiers"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "En quel année fut signé l'édit de Nantes ?",
+    answers: {
+      a: "1496",
+      b: "1598",
+      c: "1589"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Qui signa l'édit de Nantes",
+    answers: {
+      a: "Catherine de Médicis",
+      b: "Anne d'Autriche",
+      c: "Henri IV"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Comment Henri IV mit fin aux guerres de religions en France ?",
+    answers: {
+      a: "par l'extermination des protestants",
+      b: "par la mise en place d'une trève",
+      c: "par la signature de l'édit de Nantes"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Parmi ces trois rois, lequel ne mourrut pas d'assassinat ?",
+    answers: {
+      a: "Henri III",
+      b: "Henri IV",
+      c: "Charles IX"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Sous quel règne fut signé la paix des dames ?",
+    answers: {
+      a: "François Ier",
+      b: "François II",
+      c: "Louis XII"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Quand François premier adopta-t-il l'ordonnance de Villiers-Cotteret ?",
+    answers: {
+      a: "1539",
+      b: "1515",
+      c: "1519"
+    },
+    correctAnswer: "a"
   }
 ];
